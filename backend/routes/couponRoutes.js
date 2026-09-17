130428e0134db51becc12db8432e1fa34fbb07da
man31nu <mannuzadav62015@gmail.com>
2026-09-17 18:14:39 +0530

    clean: remove legacy backend files

--- backend/routes/couponRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { applyCoupon, getCoupons } = require('../controllers/couponController');
-
-const router = express.Router();
-
-router.get('/', getCoupons);
-router.post('/apply', applyCoupon);
-
-module.exports = router;
