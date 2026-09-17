c468439d791f29e3d40057095ebbd30574f9e079
man31nu <mannuzadav62015@gmail.com>
2026-09-17 15:36:26 +0530

    Added gitignore

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: '/api',
-  razorpayKey: 'rzp_test_placeholder',
+  razorpayKey: 'rzp_test_Td1GJRwld5ekBz',
   appName: 'Amrit Rasoi'
 };
