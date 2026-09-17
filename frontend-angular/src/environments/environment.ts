52be8ce5fbd0531fb8d1fcf2c82cde18b3ca0943
man31nu <mannuzadav62015@gmail.com>
2026-09-17 22:17:59 +0530

    changes

--- frontend-angular/src/environments/environment.ts
+++ frontend-angular/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://amrit-rasoi-restaurant-e-commerce-md2r.onrender.com/api',
+  apiUrl: 'https://amrit-rasoi-restaurant-e-commerce-1.onrender.com/api',
   razorpayKey: 'rzp_test_Td1GJRwld5ekBz',
   googleClientId: '399866055661-286qu7fd7jrr1u6mej7hm1ci0sk07589.apps.googleusercontent.com',
   appName: 'Amrit Rasoi'
