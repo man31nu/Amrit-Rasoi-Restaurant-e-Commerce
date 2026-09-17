141889918565f4e0297b63175050ac1d50048825
man31nu <mannuzadav62015@gmail.com>
2026-09-17 17:11:05 +0530

    env file added

--- frontend-angular/src/environments/environment.prod.ts
+++ frontend-angular/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://amrit-rasoi-restaurant-e-commerce.onrender.com/api',
   razorpayKey: 'rzp_test_Td1GJRwld5ekBz',
   googleClientId: '399866055661-286qu7fd7jrr1u6mej7hm1ci0sk07589.apps.googleusercontent.com',
   appName: 'Amrit Rasoi'
